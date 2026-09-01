c74fb6365c1193ca3ab6e431db4c1dd58dd357c2
TestingLifeLover <sdiaz336@soyudemedellin.edu.co>
2026-08-31 21:19:41 -0500

    Primer ensayo relacionado a POO con DATACLASSES

---
+++ dataclasess.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+@dataclass
+class Mascota: 
+  nombre: str
+  especie: str
+  edad: int 
+  vacunado: bool = False
+  
+  def __post_init__(self):
+    if self.nombre.strip():
+      raise ValueError("No es posible dejar el nombre en blanco")
+
+mascota1 = Mascota("Pedro", "Perro", 12, True)
+mascota2 = Mascota("", "Gato", 0)
+
+print(mascota1,mascota2)
